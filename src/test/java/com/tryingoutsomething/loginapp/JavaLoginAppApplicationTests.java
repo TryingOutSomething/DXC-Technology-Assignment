1badf8e4ca95991df1d65cc3c2f4a0ce364c4175
TryingOutSomething <jan.owyeong123@gmail.com>
2024-01-23 22:09:22 +0800

    chore: setup project

---
+++ src/test/java/com/tryingoutsomething/loginapp/JavaLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryingoutsomething.loginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
