a08e8ac594ef44d35f5a67b133da4ccffaf8bb23
TryingOutSomething <jan.owyeong123@gmail.com>
2024-01-28 22:21:59 +0800

    feat: add webjar dependency

--- src/main/java/com/tryingoutsomething/loginapp/config/security/CredentialsSecurityConfiguration.java
+++ src/main/java/com/tryingoutsomething/loginapp/config/security/CredentialsSecurityConfiguration.java
@@ -52,7 +52,7 @@ public class CredentialsSecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/admin").hasRole("MANAGER")
                         .requestMatchers("/").hasAnyRole("MANAGER", "USER")
-                        .requestMatchers("/js.**", "/css/**", "/img/**").permitAll()
+                        .requestMatchers("/webjars/**", "/js/**", "/css/**", "/img/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin -> formLogin
